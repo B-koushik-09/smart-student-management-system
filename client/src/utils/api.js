19217606dad434937cae66ff31040f1b3c3b1939
BALLA KOUSHIK <koushikballa09@gmail.com>
2026-04-30 14:25:44 +0530

    live in vercel

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: `${baseURL}/api`,
   headers: { 'Content-Type': 'application/json' }
 });
 
